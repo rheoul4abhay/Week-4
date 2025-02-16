f4880152d780c619c69665f8efb3e11a40303ea4
Abhay Shrivastava <abhayshrivastava830@gmail.com>
2025-02-17 05:17:17 +0530

    [Abhay Shrivastava] Fix . fixed catch block and added more private field names

--- Day06-Reflection_Annotations/src/main/java/Reflection/BasicLevel/AccessPrivateField/AccessPrivateField.java
+++ Day06-Reflection_Annotations/src/main/java/Reflection/BasicLevel/AccessPrivateField/AccessPrivateField.java
@@ -9,18 +9,29 @@ public class AccessPrivateField {
             Class<?> cls = person.getClass();
 
             //To access private fields
-            Field field = cls.getDeclaredField("age");
-
+            Field ageField = cls.getDeclaredField("age");
             //Access private field dynamically
-            field.setAccessible(true); //Allow access to private field
-            System.out.println("Old age : " + field.get(person));
+            ageField.setAccessible(true); //Allow access to private field
+            System.out.println("Old age: " + ageField.get(person));
 
             //Modify private age field
-            field.set(person, 44);
-            System.out.println("Modified age : " + field.get(person));
-        } catch (Exception e){
-            System.out.println("Exeption: " + e.getMessage());
-        }
+            ageField.set(person, 44);
+            System.out.println("Modified age: " + ageField.get(person));
 
+            Field nameField = cls.getDeclaredField("name");
+            nameField.setAccessible(true);
+            System.out.println("Old name: " + nameField.get(person));
+            nameField.set(person, "Rohit");
+            System.out.println("Modified name: " + nameField.get(person));
+
+            Field salaryField = cls.getDeclaredField("salary");
+            salaryField.setAccessible(true);
+            System.out.println("Old salary: $" + salaryField.get(person));
+            salaryField.set(person, 75_000);
+            System.out.println("Modified salary: $" + salaryField.get(person));
+
+        } catch (NoSuchFieldException | IllegalAccessException e){
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
---
+++ Day06-Reflection_Annotations/src/main/java/Reflection/BasicLevel/AccessPrivateField/Person.java
@@ -0,0 +1,7 @@
+package Reflection.BasicLevel.AccessPrivateField;
+
+public class Person {
+    private int age = 22;
+    private String name = "Abhay";
+    private double salary = 50_000.0;
+}
